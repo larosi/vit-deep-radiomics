d54cc87a284403044f8b2b61befc3319486c9d82
larosi <luis@odd.co>
2024-01-29 09:25:29 -0300

    chore: change models hiperparameters

--- src/umap_cls_token.py
+++ src/umap_cls_token.py
@@ -16,7 +16,8 @@ from train_petct_conv import (NoduleClassifier,
                               TransformerNoduleClassifier,
                               PETCTDataset3D,
                               create_labelmap,
-                              get_y_true_and_pred)
+                              get_y_true_and_pred,
+                              find_divisor)
 from umap.umap_ import UMAP
 import plotly.express as px
 
@@ -36,6 +37,16 @@ df = pd.read_parquet(df_path)
 df = df[df['dataset'].isin(desired_datasets)]
 df = df[df['modality'] == modality]
 df['flip'] = df['flip'].astype(str)
+
+df['divisor'] = 1
+slices_per_patient = df.groupby(['patient_id'])['slice', 'divisor'].max()
+slices_per_patient.describe()
+slices_per_patient['divisor'] = slices_per_patient['slice'].apply(find_divisor)
+slices_per_patient = slices_per_patient['divisor'].to_dict()
+df['divisor'] = df['patient_id'].apply(lambda x: slices_per_patient[x])
+
+df['patient_id_new'] = df.apply(lambda row: f"{row['patient_id']}:{np.ceil(row['slice']/row['divisor']).astype(int)}", axis=1)
+
 df.reset_index(drop=True, inplace=True)
 
 
@@ -47,12 +58,9 @@ EGFR_encoder = OneHotEncoder(handle_unknown='ignore')
 EGFR_encoder.fit(np.array(list(EGFR_lm.keys())).reshape(-1, 1))
     
 
-
+#  TODO: read model parameters from a conf file
 if modality == 'ct':
     batch_size = 1  # TODO: add support for bigger batches using zero padding to create batches of the same size
-    start_epoch = 0
-    num_epochs = 25
-    learning_rate = 0.0001
     num_layers = 1
 
     if backbone == 'medsam':
@@ -70,15 +78,23 @@ if modality == 'ct':
         num_heads = 12
         dim_feedforward = feature_dim*2
 
-else:  # TODO: PET create CNN arch for PET
-    batch_size = 32
-    start_epoch = 0
-    num_epochs = 35
-    feature_dim = 256
-    learning_rate = 0.001
-    num_layers = 3
-    num_heads = 4
-    dim_feedforward = feature_dim*4
+else:
+    num_layers = 1
+
+    if backbone == 'medsam':
+        feature_dim = 256
+        div = 2  # reduction factor of the conv layers
+        num_heads = 16
+        dim_feedforward = feature_dim*2
+
+    else:  # dinov2
+        feature_dim = 384
+        div = 3  # reduction factor of the conv layers
+        # FIXME: deprecated transformer params
+        num_heads = 12
+        dim_feedforward = feature_dim*2
+
+
 # Create model instance
 device = f'cuda:{torch.cuda.current_device()}'
 if arch == 'transformer':
@@ -91,10 +107,8 @@ else:
     model = NoduleClassifier(input_dim=feature_dim, num_classes=len(EGFR_lm), div=div)
 
 kfold = '3'
-epoch = 18
+epoch = 6
 
-kfold = '0'
-epoch = 8
 
 model.load_checkpoint(os.path.join(models_save_dir, modality, f'kfold_{kfold}'), epoch)
 model = model.to(device)
@@ -128,8 +142,7 @@ with torch.no_grad():
 y_true = np.concatenate(y_true)
 y_score = np.concatenate(y_score)
 y_pred = (y_score[:, 1] > 0.5)*1
-if arch=='transformer':
-    embeddings = [f[0,:] for f in embeddings]
+
 embeddings = np.stack(embeddings, axis=0)
 
 umap = UMAP(n_neighbors=8, min_dist=0.0, n_components=3, random_state=42, metric='l2', n_epochs=500)
@@ -142,9 +155,11 @@ df_umap['y_true'] = df_umap['y_true'].astype(str)
 df_umap['y_pred'] = df_umap['y_pred'].astype(str)
 
 df_umap[['umap_x', 'umap_y', 'umap_z']] = umap_features
+df_umap['id'] = df['patient_id_new'].unique()
+df_umap['id'] = df_umap['id'].str.split(':').str[0]
 
-fig = px.scatter_3d(df_umap, x='umap_x', y='umap_y', z='umap_z', color='y_true')
-fig.write_html(os.path.join('..', 'plots', f'{backbone}_{arch}_{arg_dataset}_umap.html'))
+fig = px.scatter_3d(df_umap, x='umap_x', y='umap_y', z='umap_z', color='y_true', symbol='id')
+fig.write_html(os.path.join('..', 'plots', f'{backbone}_{arch}_{arg_dataset}_{modality}_umap.html'))
 
 
 
