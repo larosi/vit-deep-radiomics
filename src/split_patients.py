edcc00d6a24e5298bbd8979e37327e32f5df220c
larosi <luis@odd.co>
2024-02-21 15:22:50 -0300

    fix: skip patients without spatial resolution

--- src/split_patients.py
+++ src/split_patients.py
@@ -14,14 +14,15 @@ from config_manager import get_project_dir
 
 project_dir = get_project_dir()
 df_path = os.path.join(project_dir, 'data', 'features', 'petct.parquet')
-
+drop_patients = ['AMC-022', 'R01-064']
 df = pd.read_parquet(df_path)
 df = df[df['modality'] == 'ct']
+df = df[~df['patient_id'].isin(drop_patients)]
 df.reset_index(inplace=True, drop=True)
 kfold_patients = {}
 skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
 for dataset in df['dataset'].unique():
-    patients_labels = df[df['dataset']==dataset].groupby(['patient_id'])['label'].first()
+    patients_labels = df[df['dataset'] == dataset].groupby(['patient_id'])['label'].first()
     patients = patients_labels.index.to_list()
     patients_labels = patients_labels.to_list()
     kfold_patients[dataset] = {}
