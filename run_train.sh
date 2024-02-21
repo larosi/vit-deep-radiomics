5b658818aa17562e99a1b309ba69c5b4695e51da
larosi <luis@odd.co>
2024-02-21 16:00:26 -0300

    feat: add run_train.sh

---
+++ run_train.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd ./src/
+python ./train_models.py --arch "transformer" --dataset "stanford" --modality "petct"
+python ./train_models.py --arch "transformer" --dataset "santa_maria" --modality "petct"
