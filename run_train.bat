c7e6e4861abccfd4a6b208a5c3a9ff5cf333eb13
larosi <luis@odd.co>
2024-02-21 15:25:01 -0300

    chore: update .bat

--- run_train.bat
+++ run_train.bat
@@ -1,9 +1,3 @@
 cd .\src\
-python .\train_petct_conv.py --arch "conv" --dataset "stanford" --modality "ct"
-python .\train_petct_conv.py --arch "conv" --dataset "santa_maria" --modality "ct"
-python .\train_petct_conv.py --arch "transformer" --dataset "stanford" --modality "ct"
-python .\train_petct_conv.py --arch "transformer" --dataset "santa_maria" --modality "ct"
-python .\train_petct_conv.py --arch "conv" --dataset "stanford" --modality "pet"
-python .\train_petct_conv.py --arch "conv" --dataset "santa_maria" --modality "pet"
-python .\train_petct_conv.py --arch "transformer" --dataset "stanford" --modality "pet"
-python .\train_petct_conv.py --arch "transformer" --dataset "santa_maria" --modality "pet"
+python .\train_models.py --arch "transformer" --dataset "stanford" --modality "petct"
+python .\train_models.py --arch "transformer" --dataset "santa_maria" --modality "petct"
