91ee8cc500ebc83b11edb8089758feaff3100306
larosi <luis@odd.co>
2024-02-21 15:46:40 -0300

    feat: display split in cloud compare

--- src/visualize_point_clouds.py
+++ src/visualize_point_clouds.py
@@ -26,22 +26,22 @@ def export_to_cloud_compare(df, patients, offset=100):
             df_sample_path = os.path.join('..', 'data', 'points', dataset, modality, f'{patient_id}_{label}.txt')
             df_sample[df_sample['modality'] == modality][['x', 'y', 'z', 'grey']].to_csv(df_sample_path, sep=' ', index=False)
 
+
 def export_umap_to_cloud_compare(df, df_umap, dataset, modality='ct', offset=10, use_2D=False, to_sketchfab=False):
+    df_umap = df_umap.groupby('patient_id').mean()
     if use_2D:
         pca = PCA(n_components=2)
-
-        df_umap = df_umap.groupby('patient_id').mean()
         df_umap[['x', 'y']] = pca.fit_transform(df_umap.values)
         df_umap['z'] = 0
     else:
         df_umap[['x', 'y', 'z']] = df_umap[['umap_x', 'umap_y', 'umap_z']]
 
     # scale distances to avoid overlap between patients
-    distances = pairwise_distances(df_umap.values)
+    distances = pairwise_distances(df_umap[['x', 'y', 'z']].values)
     min_distance_idx = np.unravel_index(np.argmin(distances), distances.shape)
     min_distance = distances[min_distance_idx]
     scale_factor = offset / min_distance
-    df_umap *= scale_factor
+    df_umap[['x', 'y', 'z']] *= scale_factor
     df_umap.sort_index(inplace=True)
 
     df = df[df['modality'] == modality]
@@ -57,7 +57,8 @@ def export_umap_to_cloud_compare(df, df_umap, dataset, modality='ct', offset=10,
         df[['x', 'y', 'z', 'grey', 'grey', 'grey']].astype(int).to_csv(umap_cc_path, sep=' ', index=False, header=False)
     else:
         umap_cc_path = os.path.join('..', 'data', 'points', f'{dataset}_umap.txt')
-        df[['x', 'y', 'z', 'grey', 'label']].to_csv(umap_cc_path, sep=' ', index=False)
+        df[['x', 'y', 'z', 'grey', 'label', 'is_test']].to_csv(umap_cc_path, sep=' ', index=False)
+
 
 def pairwise_distances(points):
     num_points = points.shape[0]
@@ -84,7 +85,10 @@ if __name__ == "__main__":
     export_to_cloud_compare(df, patients, offset=100)
 
     df_umap = pd.read_parquet(os.path.join('..', 'data', 'petct_embeddings_umap.parquet'))
+    df_umap = df_umap[['patient_id', 'umap_x', 'umap_y', 'umap_z', 'split']]
+    test_patients = df_umap[df_umap['split'] == 'test']['patient_id'].unique()
     df_umap = df_umap[['patient_id', 'umap_x', 'umap_y', 'umap_z']]
     df_umap = df_umap[df_umap['patient_id'].isin(patients['patient_id'].values)]
 
-    export_umap_to_cloud_compare(df, df_umap, dataset, modality='ct', offset=10, use_2D=True, to_sketchfab=False)
+    df['is_test'] = df['patient_id'].isin(test_patients) * 1
+    export_umap_to_cloud_compare(df, df_umap, dataset, modality='ct', offset=10, use_2D=False, to_sketchfab=False)
