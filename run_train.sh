56c90c451368bd3f7c4780898e1aec917c2e605c
larosi <luis@odd.co>
2024-02-28 15:34:36 -0300

    chore: add some missing unimodal train

--- run_train.sh
+++ run_train.sh
@@ -2,8 +2,12 @@
 
 cd ./src/
 python ./train_models.py --arch "conv" --dataset "stanford" --modality "pet" --gpu 1
+python ./train_models.py --arch "conv" --dataset "stanford" --modality "ct" --gpu 1
+python ./train_models.py --arch "conv" --dataset "santa_maria" --modality "pet" --gpu 1
 python ./train_models.py --arch "conv" --dataset "santa_maria" --modality "ct" --gpu 1
 python ./train_models.py --arch "transformer" --dataset "stanford" --modality "pet" --gpu 1
+python ./train_models.py --arch "transformer" --dataset "stanford" --modality "ct" --gpu 1
+python ./train_models.py --arch "transformer" --dataset "santa_maria" --modality "pet" --gpu 1
 python ./train_models.py --arch "transformer" --dataset "santa_maria" --modality "ct" --gpu 1
 python ./train_models.py --arch "transformer" --dataset "stanford" --modality "petct" --gpu 1
 python ./train_models.py --arch "transformer" --dataset "santa_maria" --modality "petct" --gpu 1
