0d9250dfecb3ace43fcff208e07b8299f5efabf8
larosi <luis@odd.co>
2024-02-29 09:43:32 -0300

    chore: add loss func arg

--- run_train.sh
+++ run_train.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 cd ./src/
-python ./train_models.py --arch "conv" --dataset "stanford" --modality "pet" --gpu 1
-python ./train_models.py --arch "conv" --dataset "stanford" --modality "ct" --gpu 1
-python ./train_models.py --arch "conv" --dataset "santa_maria" --modality "pet" --gpu 1
-python ./train_models.py --arch "conv" --dataset "santa_maria" --modality "ct" --gpu 1
-python ./train_models.py --arch "transformer" --dataset "stanford" --modality "pet" --gpu 1
-python ./train_models.py --arch "transformer" --dataset "stanford" --modality "ct" --gpu 1
-python ./train_models.py --arch "transformer" --dataset "santa_maria" --modality "pet" --gpu 1
-python ./train_models.py --arch "transformer" --dataset "santa_maria" --modality "ct" --gpu 1
-python ./train_models.py --arch "transformer" --dataset "stanford" --modality "petct" --gpu 1
-python ./train_models.py --arch "transformer" --dataset "santa_maria" --modality "petct" --gpu 1
+python ./train_models.py --arch "conv" --dataset "stanford" --modality "pet" --gpu 1 --loss "focal"
+python ./train_models.py --arch "conv" --dataset "stanford" --modality "ct" --gpu 1 --loss "focal"
+python ./train_models.py --arch "conv" --dataset "santa_maria" --modality "pet" --gpu 1 --loss "focal"
+python ./train_models.py --arch "conv" --dataset "santa_maria" --modality "ct" --gpu 1 --loss "focal"
+python ./train_models.py --arch "transformer" --dataset "stanford" --modality "pet" --gpu 1 --loss "focal"
+python ./train_models.py --arch "transformer" --dataset "stanford" --modality "ct" --gpu 1 --loss "focal"
+python ./train_models.py --arch "transformer" --dataset "santa_maria" --modality "pet" --gpu 1 --loss "focal"
+python ./train_models.py --arch "transformer" --dataset "santa_maria" --modality "ct" --gpu 1 --loss "focal"
+python ./train_models.py --arch "transformer" --dataset "stanford" --modality "petct" --gpu 1 --loss "crossmodal"
+python ./train_models.py --arch "transformer" --dataset "santa_maria" --modality "petct" --gpu 1 --loss "crossmodal"
